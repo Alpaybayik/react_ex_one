8860a186fef4801e181482bdab6aef5a9b0833ff
Alpaybayik <alpaybayik@gmail.com>
2023-11-08 15:52:18 +0300

    Tailwind Kuruldu , ilk denemeler yapılıyor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
-
+import './Style.css';
 function App() {
   return (
-    <div className="App">
-      <img src={logo} />
+    <div className="Test">
+      <h2 className="bg-red-500 text-xxl font-semibold text-opacity-100 text-white">asfsadfs dasdfsad</h2>
+      <button className="flex items-center justify-center px-4 py-3 rounded-none transition-all bg-black text-white/60 hover:bg-blue-500">Kaydet</button>
     </div>
   );
 }
